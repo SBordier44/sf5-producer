a03b9e482ba56acc5e65d6107dc3b8959f9dd60d
Sylvain B <slye@nubox.fr>
2020-12-18 21:47:14 +0100

    fix: Remove default value for image product

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,7 @@
 import './styles/app.scss';
-import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
+
+$('.btn-remove-file').click(e => {
+    $(e.currentTarget.dataset.target).val('')
+})
