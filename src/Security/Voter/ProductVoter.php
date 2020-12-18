bb36676aa9634c8811f8a779f7e6922d4f205ba8
Sylvain B <slye@nubox.fr>
2020-12-18 23:10:04 +0100

    feat: Control product added to cart

--- src/Security/Voter/ProductVoter.php
+++ src/Security/Voter/ProductVoter.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Security\Voter;
 
-use App\Entity\Producer;
+use App\Entity\CartItem;
+use App\Entity\Customer;
 use App\Entity\Product;
+use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
@@ -13,21 +15,37 @@ class ProductVoter extends Voter
 {
     public const UPDATE = 'update';
     public const DELETE = 'delete';
+    public const ADD_TO_CART = 'add_to_cart';
 
     protected function supports(string $attribute, $subject): bool
     {
-        return $subject instanceof Product && in_array($attribute, [self::UPDATE, self::DELETE]);
+        return $subject instanceof Product && in_array($attribute, [self::UPDATE, self::DELETE, self::ADD_TO_CART]);
     }
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
     {
         $user = $token->getUser();
-        if (!$user instanceof Producer) {
+        if (!$user instanceof User) {
             return false;
         }
 
         /** @var Product $subject */
 
+        if ($attribute === self::ADD_TO_CART) {
+            return $user instanceof Customer && $this->voteOnAddToCart($user, $subject);
+        }
+
         return $subject->getFarm() === $user->getFarm();
+    }
+
+    private function voteOnAddToCart(Customer $customer, Product $product): bool
+    {
+        if ($customer->getCart()->count() === 0) {
+            return true;
+        }
+
+        return $customer->getCart()->map(fn(CartItem $cartItem) => $cartItem->getProduct()->getFarm())->contains(
+            $product->getFarm()
+        );
     }
 }
