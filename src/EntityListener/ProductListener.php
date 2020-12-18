10b21af8bbd6b5bfd63c7ad8b983055c94a74f1b
Slye44 <1518249+NuBOXDevCom@users.noreply.github.com>
2020-12-18 19:47:33 +0100

    Feature/product image (#14)
    * feat: Add product image
    
    * feat: Add product image
    
    * feat: Add product image

--- src/EntityListener/ProductListener.php
+++ src/EntityListener/ProductListener.php
@@ -4,6 +4,7 @@ namespace App\EntityListener;
 
 use App\Entity\Product;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Uid\Uuid;
 
 class ProductListener
 {
@@ -12,13 +13,23 @@ class ProductListener
      * @var Security
      */
     private Security $security;
+    private string $uploadWebDir;
+    private string $uploadAbsoluteDir;
 
-    public function __construct(Security $security)
+    /**
+     * ProductListener constructor.
+     * @param Security $security
+     * @param string $uploadWebDir
+     * @param string $uploadAbsoluteDir
+     */
+    public function __construct(Security $security, string $uploadWebDir, string $uploadAbsoluteDir)
     {
         $this->security = $security;
+        $this->uploadWebDir = $uploadWebDir;
+        $this->uploadAbsoluteDir = $uploadAbsoluteDir;
     }
 
-    public function prePersist(Product $product)
+    public function prePersist(Product $product): void
     {
         if ($product->getFarm() !== null) {
             return;
@@ -26,5 +37,24 @@ class ProductListener
         if ($this->security->getUser()) {
             $product->setFarm($this->security->getUser()->getFarm());
         }
+        $this->upload($product);
+    }
+
+    public function preUpdate(Product $product): void
+    {
+        $this->upload($product);
+    }
+
+    private function upload(Product $product): void
+    {
+        if ($product->getImage() === null || $product->getImage()->getFile() === null) {
+            return;
+        }
+
+        $filename = Uuid::v4() . '.' . $product->getImage()->getFile()->getClientOriginalExtension();
+
+        $product->getImage()->getFile()->move($this->uploadAbsoluteDir, $filename);
+
+        $product->getImage()->setPath($this->uploadWebDir . $filename);
     }
 }
