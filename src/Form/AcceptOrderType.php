f5bf4c75a0802883ca967c081277266c73b1012f
Slye44 <1518249+NuBOXDevCom@users.noreply.github.com>
2020-12-21 01:36:54 +0100

    feat: add accept order and slotting order (#25)

---
+++ src/Form/AcceptOrderType.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\Order;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AcceptOrderType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'slots',
+            CollectionType::class,
+            [
+                'by_reference' => false,
+                'allow_add' => true,
+                'entry_type' => SlotType::class
+            ]
+        );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('data_class', Order::class);
+    }
+}
---
+++ src/Form/SlotType.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use App\Entity\Slot;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SlotType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'startedAt',
+            DateTimeType::class,
+            [
+                'widget' => 'single_text',
+                'input' => 'datetime_immutable'
+            ]
+        );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('data_class', Slot::class);
+    }
+}
